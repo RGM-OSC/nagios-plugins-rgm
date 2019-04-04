7e7accc02781eafaa1a195938f3244306575e44e
Eric Belhomme <ebelhomme@ricozome.net>
2019-04-04 18:33:18 +0200

    release 1.1.0: - massive code rewrite in more pythonic way - perfdata in MB instead of percentage

--- SOURCES/metricbeat/disk.py
+++ SOURCES/metricbeat/disk.py
@@ -9,26 +9,31 @@ DESCRIPTION :
 
 AUTHOR :
   * Julien Dumarchey <jdumarchey@fr.scc.com>   START DATE :    Sep 04 11:00:00 2018 
-              
+  * Eric Belhomme <ebelhomme@fr.scc.com>
+  
 CHANGES :
   * VERSION     DATE        WHO                                         DETAIL
   * 0.0.1       2018-09-04  Julien Dumarchey <jdumarchey@fr.scc.com>    Initial version
   * 1.0.1       2019-03-26  Eric Belhomme <ebelhomme@fr.scc.com>        replace getopts by argparse module
                                                                         code factorization & mutualization
+  * 1.1.0       2019-04-04  Eric Belhomme <ebelhomme@fr.scc.com>        massive rewrite with list comprehension
+                                                                        perfdata are absolute MB instead of percentage                                                                        
 '''
 
 __author__ = "Julien Dumarchey, Eric Belhomme"
 __copyright__ = "2018, SCC"
 __credits__ = ["Julien Dumarchey", "Eric Belhomme"]
 __license__ = "GPL"
-__version__ = "1.0.1"
+__version__ = "1.1.0"
 __maintainer__ = "Julien Dumarchey"
 
 ## MODULES FEATURES #######################################################################################################
 
 # Import the following modules:
-import sys, re, argparse, requests, json
+import sys, re, argparse, requests, json, pprint
 from _rgmbeat import generic_api_call, generic_api_payload, get_data_validity_range, validate_elastichost
+
+NagiosRetCode = ('OK', 'WARNING', 'CRITICAL', 'UNKNOWN')
 
 # If required, disable SSL Warning Logging for "requests" library:
 #requests.packages.urllib3.disable_warnings()
@@ -67,41 +72,43 @@ def custom_api_payload(plugin_hostname,data_validity):
 def get_disk(elastichost, plugin_hostname,data_validity,verbose):
     try:
         # Get prerequisites for ElasticSearch API:
+        resp_entries_range = 0
         addr, header = generic_api_call(elastichost)
         payload = custom_api_payload(plugin_hostname,data_validity)
         # Request the ElasticSearch API:
         results = requests.get(url=addr, headers=header, json=payload, verify=False)
         results_json = results.json()
         if verbose:
-            print("## VERBOSE MODE - API REST HTTP RESPONSE: ##########################################")
-            print("request payload: {}".format(payload))
-            print("JSON output: {}".format(results_json))
+            pp = pprint.PrettyPrinter(indent=4)
+            print("### VERBOSE MODE - API REST HTTP RESPONSE: ##########################################")
+            print("### request payload:")
+            pp.pprint(payload)
+            print("### JSON output:")
+            print(results_json)
             print("####################################################################################")
-        # Extract the "Total Hit" from results (= check if Disk Value has been returned):
-        total_hit = int(results_json["hits"]["total"])
-        # If request hits: find out how many Items are required to get space for all disks:
-        if total_hit != 0:
-            disks_names_list = []
-            for index in (range(total_hit)) :
-                disk_name = results_json["hits"]["hits"][index]["_source"]["system"]["filesystem"]["device_name"]
-                if disk_name not in disks_names_list :
-                    disks_names_list.append(disk_name)
-                else :
-                    resp_entries_range = int(index)
-                    break
-        # If request hits: for each Disk, extract results (Disk Values in %) and display Verbose Mode if requested in ARGS ; otherwise return a static code (0):
-        disk_values_dic = {}
-        if total_hit != 0:
-            for position in (range(resp_entries_range)) :
-                disk_values_dic.update( {"disk_"+str(position)+"": []} )
-                disk_values_dic["disk_"+str(position)].append(results_json["hits"]["hits"][position]["_source"]["system"]["filesystem"]["device_name"])
-                disk_values_dic["disk_"+str(position)].append(results_json["hits"]["hits"][position]["_source"]["system"]["filesystem"]["mount_point"])
-                disk_values_dic["disk_"+str(position)].append(float(round(results_json["hits"]["hits"][position]["_source"]["system"]["filesystem"]["used"]["pct"] * (100),2)))
-                disk_values_dic["disk_"+str(position)].append(float(round(results_json["hits"]["hits"][position]["_source"]["system"]["filesystem"]["used"]["bytes"] / (1024*1024*1024),2)))
-                disk_values_dic["disk_"+str(position)].append(float(round(results_json["hits"]["hits"][position]["_source"]["system"]["filesystem"]["free"] / (1024*1024*1024),2)))
+
+        if int(results_json["hits"]["total"]) > 0:
+            fslist = []
+            # get a list of returned fs, then keep only latest item of each mountpoint
+            allfslist = [ i['_source'] for i in results_json['hits']['hits'] ]
+            for fs in set([ i['system']['filesystem']['mount_point'] for i in allfslist ]):
+                item = max([ i for i in allfslist if i['system']['filesystem']['mount_point'] == fs ], key=lambda timestamp: timestamp['@timestamp'])
+                
+                fslist.append({
+                    'device_name': item['system']['filesystem']['device_name'],
+                    'mount_point': item['system']['filesystem']['mount_point'],
+                    'total': int(item['system']['filesystem']['total']),
+                    'free': int(item['system']['filesystem']['free']),
+                    'used': int(item['system']['filesystem']['used']['bytes'])}
+                )
+            def sort_fslist(element):
+                return len(element['mount_point'])
+
+            return sorted(fslist, key=sort_fslist)
         else:
-            disk_values_dic.update( {"0": [0, 0, 0, 0, 0]} )
-        return total_hit, resp_entries_range, disk_values_dic
+            # no fs returned
+            return False
+
     except Exception as e:
         print("Error calling \"get_disk\"... Exception {}".format(e))
         sys.exit(3)
@@ -110,98 +117,79 @@ def get_disk(elastichost, plugin_hostname,data_validity,verbose):
 def build_alerting_list(elastichost,plugin_hostname,warning_treshold,critical_treshold,data_validity,verbose):
     try:
         # Get Disk values:
-        total_hit, resp_entries_range, disk_values_dic = get_disk(elastichost, plugin_hostname,data_validity,verbose)
-        #print("Debug - \"disk_values_dic\" -> "+str(disk_values_dic)+"")
-        # Build Alerting dic and Performance Data dic:
-        alerting_dic, perfdata_dic = {}, {}
-        for index in range(resp_entries_range) :
-            if total_hit != 0 and (disk_values_dic["disk_"+str(index)][2] >= critical_treshold) :
-                alerting_dic.update( {"CRITICAL_"+str(index)+"": ""+str(disk_values_dic["disk_"+str(index)][1])+" : "+str(disk_values_dic["disk_"+str(index)][2])+"%"} )
-                perfdata_dic.update( {"CRITICAL_"+str(index)+"": " | 'Disk "+str(disk_values_dic["disk_"+str(index)][1])+"'="+str(disk_values_dic["disk_"+str(index)][2])+";"+str(warning_treshold)+";"+str(critical_treshold)+";0;100;"} )
-            elif total_hit != 0 and (disk_values_dic["disk_"+str(index)][2]  >= warning_treshold and disk_values_dic["disk_"+str(index)][2] < critical_treshold) :
-                alerting_dic.update( {"WARNING_"+str(index)+"": ""+str(disk_values_dic["disk_"+str(index)][1])+" : "+str(disk_values_dic["disk_"+str(index)][2])+"%"} )    
-                perfdata_dic.update( {"WARNING_"+str(index)+"": " | 'Disk "+str(disk_values_dic["disk_"+str(index)][1])+"'="+str(disk_values_dic["disk_"+str(index)][2])+";"+str(warning_treshold)+";"+str(critical_treshold)+";0;100;"} )
-            elif total_hit != 0 and (disk_values_dic["disk_"+str(index)][2] < warning_treshold) :  
-                alerting_dic.update( {"OK_"+str(index)+"": ""+str(disk_values_dic["disk_"+str(index)][1])+" : "+str(disk_values_dic["disk_"+str(index)][2])+"%"} )  
-                perfdata_dic.update( {"OK_"+str(index)+"": " | 'Disk "+str(disk_values_dic["disk_"+str(index)][1])+"'="+str(disk_values_dic["disk_"+str(index)][2])+";"+str(warning_treshold)+";"+str(critical_treshold)+";0;100;"} )
-            else:     
-                alerting_dic.update( {"UNKNOWN_"+str(index)+"": "NA | NA"} ) 
-                perfdata_dic.update( {"UNKNOWN_"+str(index)+"": "NA | NA"} ) 
-        #print("Debug - \"alerting_dic\" -> "+str(alerting_dic)+"")
-        #print("Debug - \"perfdata_dic\" -> "+str(perfdata_dic)+"")
-        
-        # Parse Alerting dic for storing System Information sorted by Severity in relevant lists:
-        rgm_info_critical, rgm_info_warning, rgm_info_ok, rgm_info_unknown = [], [], [], []
-        rgm_perfdata_critical, rgm_perfdata_warning, rgm_perfdata_ok, rgm_perfdata_unknown = [], [], [], []
-        for index in range(resp_entries_range) :
-            if ("UNKNOWN_"+str(index)+"") in alerting_dic :
-                rgm_info_unknown.append(alerting_dic["UNKNOWN_"+str(index)+""])
-                rgm_perfdata_unknown.append(perfdata_dic["UNKNOWN_"+str(index)+""])
-            if ("CRITICAL_"+str(index)+"") in alerting_dic :
-                rgm_info_critical.append(alerting_dic["CRITICAL_"+str(index)+""])
-                rgm_perfdata_critical.append(perfdata_dic["CRITICAL_"+str(index)+""])
-            if ("WARNING_"+str(index)+"") in alerting_dic :
-                rgm_info_warning.append(alerting_dic["WARNING_"+str(index)+""])
-                rgm_perfdata_warning.append(perfdata_dic["WARNING_"+str(index)+""])
-            if ("OK_"+str(index)+"") in alerting_dic :
-                rgm_info_ok.append(alerting_dic["OK_"+str(index)+""])
-                rgm_perfdata_ok.append(perfdata_dic["OK_"+str(index)+""])
-            
-        # Prepare Performance Data to be return in a compliant format to RGM:
-        rgm_perfdata_critical = str(rgm_perfdata_critical).replace("[\"", "").replace("\"]", "").replace("\", \"", "")
-        rgm_perfdata_warning = str(rgm_perfdata_warning).replace("[\"", "").replace("\"]", "").replace("\", \"", "")
-        rgm_perfdata_ok = str(rgm_perfdata_ok).replace("[\"", "").replace("\"]", "").replace("\", \"", "")
+        ret = []
+        fslist = get_disk(elastichost, plugin_hostname, data_validity, verbose)
 
-        # Return RGM System Information and Performance Data ready to display:
-        return rgm_info_critical, rgm_info_warning, rgm_info_ok, rgm_info_unknown, rgm_perfdata_critical, rgm_perfdata_warning, rgm_perfdata_ok, rgm_perfdata_unknown
+        if isinstance(fslist, list):
+            for item in fslist:
+                item['warn_treshold_abs'] = (warning_treshold * item['total']) / 100
+                item['crit_treshold_abs'] = (critical_treshold * item['total']) / 100
+                item['nagios_status'] = 3
+
+                if item['used'] >= item['crit_treshold_abs']:
+                    item['nagios_status'] = 2
+                elif item['used'] >= item['warn_treshold_abs']:
+                    item['nagios_status'] = 1
+                elif item['used'] < item['warn_treshold_abs']:
+                    item['nagios_status'] = 0
+
+                ret.append(item)
+            return ret
+        else:
+            return False
 
     except Exception as e:
         print("Error calling \"build_alerting_list\"... Exception {}".format(e))
         sys.exit(3)
 
+def byte2mbyte(bytes):
+    return int(bytes/(1024*1024))
+
 # Display Disk space (System Information + Performance Data) in a format compliant with RGM expectations:
 def rgm_disk_output(elastichost, plugin_hostname,warning_treshold,critical_treshold,data_validity,verbose):
+
     try:
-        # Get "ready to display" System Information and Performance Data:
-        rgm_info_critical, rgm_info_warning, rgm_info_ok, rgm_info_unknown, \
-            rgm_perfdata_critical, rgm_perfdata_warning, rgm_perfdata_ok, \
-            rgm_perfdata_unknown = build_alerting_list(elastichost, plugin_hostname,warning_treshold,critical_treshold,data_validity,verbose)
-        # Return System Information + Performance Data in a format compliant with RGM expectations:
-        if rgm_info_unknown:
-            print("UNKNOWN: Disk space value has not been returned...")
-            sys.exit(3)
-        elif (rgm_info_critical) and (not rgm_info_warning) and (not rgm_info_ok):
-            #print("Debug - \"rgm_disk_output\" -> Scenario: \"1 0 0\"")
-            print("CRITICAL: "+str(rgm_info_critical)+""+str(rgm_perfdata_critical))
-            sys.exit(2)
-        elif (not rgm_info_critical) and (rgm_info_warning) and (not rgm_info_ok):
-            #print("Debug - \"rgm_disk_output\" -> Scenario: \"0 1 0\"")
-            print("WARNING: "+str(rgm_info_warning)+""+str(rgm_perfdata_warning))
-            sys.exit(1)
-        elif (not rgm_info_critical) and (not rgm_info_warning) and (rgm_info_ok):
-            #print("Debug - \"rgm_disk_output\" -> Scenario: \"0 0 1\"")
-            print("OK: "+str(rgm_info_ok)+""+str(rgm_perfdata_ok))
-            sys.exit(0)
-        elif (not rgm_info_critical) and (not rgm_info_warning) and (not rgm_info_ok):
-            #print("Debug - \"rgm_disk_output\" -> Scenario: \"0 0 0\"")
-            print("UNKNOWN: Disk space value has not been returned...")
-            sys.exit(3)
-        elif (rgm_info_critical) and (rgm_info_warning) and (rgm_info_ok):
-            #print("Debug - \"rgm_disk_output\" -> Scenario: \"1 1 1\"")
-            print("CRITICAL: "+str(rgm_info_critical)+", WARNING: "+str(rgm_info_warning)+""+str(rgm_perfdata_critical)+""+str(rgm_perfdata_warning))
-            sys.exit(2)
-        elif (rgm_info_critical) and (rgm_info_warning) and (not rgm_info_ok):
-            #print("Debug - \"rgm_disk_output\" -> Scenario: \"1 1 0\"")
-            print("CRITICAL: "+str(rgm_info_critical)+", WARNING: "+str(rgm_info_warning)+""+str(rgm_perfdata_critical)+""+str(rgm_perfdata_warning))
-            sys.exit(2)
-        elif (not rgm_info_critical) and (rgm_info_warning) and (rgm_info_ok):
-            #print("Debug - \"rgm_disk_output\" -> Scenario: \"0 1 1\"")
-            print("WARNING: "+str(rgm_info_warning)+""+str(rgm_perfdata_warning))
-            sys.exit(1)
-        elif (rgm_info_critical) and (not rgm_info_warning) and (rgm_info_ok):
-            #print("Debug - \"rgm_disk_output\" -> Scenario: \"1 0 1\"")
-            print("CRITICAL: "+str(rgm_info_critical)+""+str(rgm_perfdata_critical))
-            sys.exit(2)
+        nagios_status = 3
+        fslist = build_alerting_list(elastichost, plugin_hostname,warning_treshold,critical_treshold,data_validity,verbose)
+        if isinstance(fslist, list):
+            nagios_status = max(fslist, key=lambda status: status['nagios_status'])['nagios_status']
+            outtext = []
+            outperf = []
+            if nagios_status != 0:
+                crit = [ i['mount_point'] for i in fslist if i['nagios_status'] == 2 ]
+                if len(crit) > 0:
+                    outtext.append("{} mountpoints in CRITICAL state ({}%): {}".format(
+                        str(len(crit)),
+                        str(critical_treshold),
+                        ", ".join(crit)))
+                warn = [ i['mount_point'] for i in fslist if i['nagios_status'] == 1 ]
+                if len(warn) > 0:
+                    outtext.append("{} mountpoints in WARNING state ({}%): {}".format(
+                        str(len(warn)),
+                        str(warning_treshold),
+                        ", ".join(warn)))
+            else:
+                outtext.append("All mountpoints in OK state ({}%, {}%)".format(int(warning_treshold), int(critical_treshold)))
+
+            for item in fslist:
+                outperf.append("'{label}'={value}MB;{warn};{crit};0;{total}".format(
+                    label=item['mount_point'],
+                    value=str(byte2mbyte(item['used'])),
+                    warn=str(byte2mbyte(item['warn_treshold_abs'])),
+                    crit=str(byte2mbyte(item['crit_treshold_abs'])),
+                    total=str(byte2mbyte(item['total']))
+                ))
+
+        else:
+            print("{}: no output returned for time period ({} min)".format(NagiosRetCode[nagios_status], data_validity))
+
+        print("{}: {} | {}".format(
+            NagiosRetCode[nagios_status],
+            " ".join(outtext),
+            " ".join(outperf)
+        ))
+        sys.exit(nagios_status)
+
     except Exception as e:
         print("Error calling \"rgm_disk_output\"... Exception {}".format(e))
         sys.exit(3)
