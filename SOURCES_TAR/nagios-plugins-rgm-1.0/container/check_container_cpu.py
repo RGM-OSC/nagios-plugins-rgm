16ab076549fc55e82ced287306b9ebec396a1f45
Eric Belhomme <ebelhomme@fr.scc.com>
2020-10-22 16:14:42 +0000

    add verbose levels for Nagios output text add storage unit autodetection (or force specific unit)

--- SOURCES_TAR/nagios-plugins-rgm-1.0/container/check_container_cpu.py
+++ SOURCES_TAR/nagios-plugins-rgm-1.0/container/check_container_cpu.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 '''
@@ -22,132 +22,164 @@ __license__ = "GPL"
 __version__ = "0.0.1"
 __maintainer__ = "Lucas Fueyo"
 
-## MODULES FEATURES #######################################################################################################
+# MODULES FEATURES ####################################################################################################
 
 # Import the following modules:
-import sys, re, argparse, requests, json, urllib3, urllib, time
+import sys
+import argparse
+import requests
+import urllib
+import time
 
 NagiosRetCode = ('OK', 'WARNING', 'CRITICAL', 'UNKNOWN')
 
-## Declare Functions ######################################################################################################
+# Declare Functions ###################################################################################################
+
 
 # Build a custom URL for Prometheus (Here: HTTP uri for getting CPU usage values)
 def get_custom_api_url(prometheus_host, container_image, container_label, interval_start):
 
-	# Check interval value and deduce step and interval_end from it
-	if isinstance(interval_start, int):
-		interval_end = int(time.time())
-		step = str(interval_end - interval_start)
-	else:
-		print("Error - the starting date is not a timestamp in seconds")
-		sys.exit(3)
+    # Check interval value and deduce step and interval_end from it
+    if isinstance(interval_start, int):
+        interval_end = int(time.time())
+        step = str(interval_end - interval_start)
+    else:
+        print("Error - the starting date is not a timestamp in seconds")
+        sys.exit(3)
 
-	prometheus_query = urllib.quote("sum(rate(container_cpu_usage_seconds_total{" + container_label + "=~\".*" + container_image + ".*\"}[" + step + "s]))*100")
+    prometheus_query = urllib.parse.quote(
+        "sum(rate(container_cpu_usage_seconds_total{" + container_label + "=~\".*" +
+        container_image + ".*\"}[" + step + "s]))*100"
+    )
 
-	# Create object to return
-	request_url = str(prometheus_host + "/api/v1/query_range?query=" + prometheus_query + "&start=" + str(interval_start) + "&end=" + str(interval_end) + "&step=" + step)
-	
-	return request_url
+    # Create object to return
+    request_url = str(
+        prometheus_host + "/api/v1/query_range?query=" + prometheus_query + "&start=" +
+        str(interval_start) + "&end=" + str(interval_end) + "&step=" + step
+    )
+
+    return request_url
+
 
 # Request a custom Prometheus API Rest Call (Here : Get CPU : Used)
 def get_cpu(prometheus_host, container_image, container_label, interval_start, verbose):
 
-	try:
-		# Get the request url
-		request_url = get_custom_api_url(prometheus_host, container_image, container_label, interval_start)
+    try:
+        # Get the request url
+        request_url = get_custom_api_url(prometheus_host, container_image, container_label, interval_start)
 
-		# Request the Prometheus API
-		results = requests.get(url=request_url)
-		results_json = results.json()
+        # Request the Prometheus API
+        results = requests.get(url=request_url)
+        results_json = results.json()
 
-		if verbose:
-			print("## VERBOSE MODE - API REST HTTP RESPONSE: ##########################################")
-			print("JSON output: {}".format(results_json))
-			print("####################################################################################")
+        if verbose:
+            print("## VERBOSE MODE - API REST HTTP RESPONSE: ##########################################")
+            print("JSON output: {}".format(results_json))
+            print("####################################################################################")
 
-		# Check if json contains values
-		if results_json['data']['result']:
-		
-			# Get the last metric available 
-			cpu_used_pct = float(results_json['data']['result'][-1]['values'][-1][1])
+        # Check if json contains values
+        if results_json['data']['result']:
 
-		else:
-			cpu_used_pct = -1
+            # Get the last metric available
+            cpu_used_pct = float(results_json['data']['result'][-1]['values'][-1][1])
 
-		return cpu_used_pct
+        else:
+            cpu_used_pct = -1
 
-	except Exception as e:
-		print("Error calling \"get_cpu\"... Exception {}".format(e))
-		sys.exit(3)
+        return cpu_used_pct
+
+    except Exception as e:
+        print("Error calling \"get_cpu\"... Exception {}".format(e))
+        sys.exit(3)
+
 
 # Display CPU usage in a format compliant with RGM expectations
-def rgm_cpu_output(prometheus_host, container_image, container_label, interval_start, warning_threshold, critical_threshold, verbose):
+def rgm_cpu_output(
+    prometheus_host, container_image, container_label, interval_start, warning_threshold, critical_threshold, verbose
+):
 
-	try:
-		
-		retcode = 3
-		# Get CPU usage value
-		cpu_used_pct = get_cpu(prometheus_host, container_image, container_label, interval_start, verbose)
+    try:
 
-		if cpu_used_pct == -1:
-			print("UNKNOWN: CPU has not been returned...")
-			sys.exit(retcode)
-		elif cpu_used_pct >= critical_threshold:
-			retcode = 2
-		elif cpu_used_pct >= warning_threshold:
-			retcode = 1
-		elif cpu_used_pct < warning_threshold:
-			retcode = 0
+        retcode = 3
+        # Get CPU usage value
+        cpu_used_pct = get_cpu(prometheus_host, container_image, container_label, interval_start, verbose)
 
-		print("{rc} - CPU Usage : {cpu}% |" \
-				" CPU={cpu};{wt};{ct};0;100".format(
-				rc=NagiosRetCode[retcode],
-				cpu=str(round(cpu_used_pct,2)),
-				wt=warning_threshold,
-				ct=critical_threshold))
-		exit(retcode)
+        if cpu_used_pct == -1:
+            print("UNKNOWN: CPU has not been returned...")
+            sys.exit(retcode)
+        elif cpu_used_pct >= critical_threshold:
+            retcode = 2
+        elif cpu_used_pct >= warning_threshold:
+            retcode = 1
+        elif cpu_used_pct < warning_threshold:
+            retcode = 0
 
-	except Exception as e:
-		print("Error calling \"rgm_cpu_output\"... Exception {}".format(e))
-		sys.exit(3)
+        print(
+            "{rc} - CPU Usage : {cpu}% |"
+            " CPU={cpu};{wt};{ct};0;100".format(
+                rc=NagiosRetCode[retcode],
+                cpu=str(round(cpu_used_pct, 2)),
+                wt=warning_threshold,
+                ct=critical_threshold
+            )
+        )
+        exit(retcode)
 
-## Get Options/Arguments then Run Script ##################################################################################
+    except Exception as e:
+        print("Error calling \"rgm_cpu_output\"... Exception {}".format(e))
+        sys.exit(3)
 
+
+# Get Options/Arguments then Run Script ###############################################################################
 if __name__ == '__main__':
 
-	parser = argparse.ArgumentParser(description="""
-	* Nagios plugin used to return container CPU usage from Prometheus.
-	* CPU values are pushed from node-exporter and cadvisor installed on the monitored container environment.
-	* CPU request is handled by API REST againt Prometheus.
-	""",
-	usage="""
-	Get CPU usage for container "ct1" from timestamp (default 1min) to now
+    parser = argparse.ArgumentParser(
+        description="""
+        * Nagios plugin used to return container CPU usage from Prometheus.
+        * CPU values are pushed from node-exporter and cadvisor installed on the monitored container environment.
+        * CPU request is handled by API REST againt Prometheus.
+        """,
+        usage="""
+        Get CPU usage for container "ct1" from timestamp (default 1min) to now
 
-	Warning alert if CPU > 80%%.
-	Critical alert if CPU > 95%%.
+        Warning alert if CPU > 80%%.
+        Critical alert if CPU > 95%%.
 
-	python check_container_cpu.py -H 'image-front' -w 80 -c 95
+        python check_container_cpu.py -H 'image-front' -w 80 -c 95
 
-	Get CPU for container deployed from image "image-front" with Verbose mode enabled.
+        Get CPU for container deployed from image "image-front" with Verbose mode enabled.
 
-	python check_container_cpu.py -H 'image-front' -w 85 -c 95 -v
+        python check_container_cpu.py -H 'image-front' -w 85 -c 95 -v
 
-	Get CPU for container matching label 'monitoring = mySpecificAppContainer'
+        Get CPU for container matching label 'monitoring = mySpecificAppContainer'
 
-	python check_container_cpu.py -H mySpecificAppContainer -l monitoring 
-	""",
-	epilog="version {}, copyright {}".format(__version__, __copyright__))
+        python check_container_cpu.py -H mySpecificAppContainer -l monitoring
+        """,
+        epilog="version {}, copyright {}".format(__version__, __copyright__)
+    )
 
-	parser.add_argument('-H', '--hostname', type=str, help='Container image name', required=True)
-	parser.add_argument('-l', '--label', type=str, help='Container label name', default='image')
-	parser.add_argument('-w', '--warning', type=int, help='warning trigger threshold', default='80')
-	parser.add_argument('-c', '--critical', type=int, help='critical trigger threshold', default='95')
-	parser.add_argument('-i', '--intervalstart', type=int, help='timestamp value in seconds to start the CPU usage check - default to 6min', default=int(time.time())-60)
-	parser.add_argument('-E', '--prometheushost', type=str, help='connection URL of Prometheus server', default="http://localhost:9090")
-	parser.add_argument('-v', '--verbose', help='be verbose', action='store_true')
+    parser.add_argument('-H', '--hostname', type=str, help='Container image name', required=True)
+    parser.add_argument('-l', '--label', type=str, help='Container label name', default='image')
+    parser.add_argument('-w', '--warning', type=int, help='warning trigger threshold', default='80')
+    parser.add_argument('-c', '--critical', type=int, help='critical trigger threshold', default='95')
+    parser.add_argument(
+        '-i', '--intervalstart', type=int,
+        help='timestamp value in seconds to start the CPU usage check - default to 6min',
+        default=int(time.time())-60
+    )
+    parser.add_argument(
+        '-E', '--prometheushost', type=str, help='connection URL of Prometheus server',
+        default="http://localhost:9090"
+    )
+    parser.add_argument('-v', '--verbose', help='be verbose', action='store_true')
 
-	args = parser.parse_args()
-
-	rgm_cpu_output(args.prometheushost, args.hostname, args.label, args.intervalstart, args.warning, args.critical, args.verbose)
-
-#EOF
+    args = parser.parse_args()
+    rgm_cpu_output(
+        args.prometheushost,
+        args.hostname,
+        args.label,
+        args.intervalstart,
+        args.warning,
+        args.critical,
+        args.verbose
+    )
--- SOURCES_TAR/nagios-plugins-rgm-1.0/metricbeat/interfaces.py
+++ SOURCES_TAR/nagios-plugins-rgm-1.0/metricbeat/interfaces.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 '''
@@ -7,11 +7,12 @@ DESCRIPTION :
 
 AUTHOR :
   * Eric Belhomme <ebelhomme@fr.scc.com>
-  
+
 CHANGES :
   * VERSION     DATE        WHO                                         DETAIL
   * 0.0.1       2019-04-04  Eric Belhomme <ebelhomme@fr.scc.com>        Initial version
-  * 0.0.2       2019-08-14  Samuel Ronciaux <sronciaux@fr.scc.com>      change metricset variable name to metricbeat agent 7.2.x  
+  * 0.0.2       2019-08-14  Samuel Ronciaux <sronciaux@fr.scc.com>      change metricset variable name to metricbeat
+                                                                        agent 7.2.x
 '''
 
 __author__ = "Eric Belhomme"
@@ -21,23 +22,26 @@ __license__ = "GPL"
 __version__ = "0.0.2"
 __maintainer__ = "Eric Belhomme"
 
-## MODULES FEATURES #######################################################################################################
+# MODULES FEATURES ####################################################################################################
 
 # Import the following modules:
-import sys, re, argparse, requests, json, pprint
+import sys
+import argparse
+import requests
+import pprint
 from _rgmbeat import generic_api_call, generic_api_payload, get_data_validity_range, validate_elastichost
 
 NagiosRetCode = ('OK', 'WARNING', 'CRITICAL', 'UNKNOWN')
 
 
-# If required, disable SSL Warning Logging for "requests" library:
-#requests.packages.urllib3.disable_warnings()
-
-## Declare Functions ######################################################################################################
+# Declare Functions ###################################################################################################
 
 
-# Build a custom Payload for ElasticSearch (here: HTTP Request Body for getting Disk space values for a specified hostname):
 def custom_api_payload(hostname, data_validity):
+    '''
+    Build a custom Payload for ElasticSearch
+    here: HTTP Request Body for getting Disk space values for a specified hostname
+    '''
     try:
         # ElasticSearch Custom Variables:
         metricset_module = "system"
@@ -50,13 +54,19 @@ def custom_api_payload(hostname, data_validity):
         custom_payload = generic_api_payload(40)
 
         # Add the Query structure with ElasticSearch Variables:
-        custom_payload.update( {'query': {'bool': {'must': [], 'filter': [], 'should': [], 'must_not': [] }}} )
-        custom_payload['query']['bool']['must'].append( {'match_all': {}} )
-        custom_payload['query']['bool']['must'].append( {'match_phrase': {'event.module': {'query': metricset_module}}} )
-        custom_payload['query']['bool']['must'].append( {'match_phrase': {'metricset.name': {'query': metricset_name}}} )
-        custom_payload['query']['bool']['must'].append( {'match_phrase': {'host.name': {'query': hostname}}} )
-        custom_payload['query']['bool']['must_not'].append( {'match': {'system.network.name': {'query': 'lo'}}} )
-        custom_payload['query']['bool']['must'].append( {'range': {'@timestamp': {'gte': str(oldest_valid_timestamp), 'lte': str(newest_valid_timestamp), 'format': 'epoch_millis'}}} )
+        custom_payload.update({'query': {'bool': {'must': [], 'filter': [], 'should': [], 'must_not': []}}})
+        custom_payload['query']['bool']['must'].append({'match_all': {}})
+        custom_payload['query']['bool']['must'].append({'match_phrase': {'event.module': {'query': metricset_module}}})
+        custom_payload['query']['bool']['must'].append({'match_phrase': {'metricset.name': {'query': metricset_name}}})
+        custom_payload['query']['bool']['must'].append({'match_phrase': {'host.name': {'query': hostname}}})
+        custom_payload['query']['bool']['must_not'].append({'match': {'system.network.name': {'query': 'lo'}}})
+        custom_payload['query']['bool']['must'].append(
+            {'range': {'@timestamp': {
+                'gte': str(oldest_valid_timestamp),
+                'lte': str(newest_valid_timestamp),
+                'format': 'epoch_millis'
+            }}}
+        )
         return custom_payload
     except Exception as e:
         print("Error calling \"custom_api_payload\"... Exception {}".format(e))
@@ -68,13 +78,13 @@ def get_interfaces(elastichost, hostname, data_validity, verbose):
     try:
         # Get prerequisites for ElasticSearch API:
         addr, header = generic_api_call(elastichost)
-        payload = custom_api_payload(hostname,data_validity)
+        payload = custom_api_payload(hostname, data_validity)
         # Request the ElasticSearch API:
         results = requests.get(url=addr, headers=header, json=payload, verify=False)
         results_json = results.json()
         if verbose:
             pp = pprint.PrettyPrinter(indent=4)
-            print("### VERBOSE MODE - API REST HTTP RESPONSE: ##########################################")
+            print("### VERBOSE MODE - API REST HTTP RESPONSE: #########################################")
             print("### request payload:")
             pp.pprint(payload)
             print("### JSON output:")
@@ -84,11 +94,14 @@ def get_interfaces(elastichost, hostname, data_validity, verbose):
         if not bool(results_json['timed_out']) and int(results_json["hits"]["total"]['value']) > 0:
             niclst = []
             # get a list of returned fs, then keep only latest item of each mountpoint
-            allfslist = [ i['_source'] for i in results_json['hits']['hits'] ]
-            for nicname in set([ i['system']['network']['name'] for i in allfslist ]):
-                item = max([ i for i in allfslist if i['system']['network']['name'] == nicname  ], key=lambda timestamp: timestamp['@timestamp'])
-                
+            allfslist = [i['_source'] for i in results_json['hits']['hits']]
+            for nicname in set([i['system']['network']['name'] for i in allfslist]):
+                item = max(
+                    [i for i in allfslist if i['system']['network']['name'] == nicname],
+                    key=lambda timestamp: timestamp['@timestamp']
+                )
                 niclst.append(item['system']['network'])
+
             def sort_list(element):
                 return len(element['name'])
 
@@ -102,29 +115,43 @@ def get_interfaces(elastichost, hostname, data_validity, verbose):
         sys.exit(3)
 
 
-## Get Options/Arguments then Run Script ##################################################################################
+# Get Options/Arguments then Run Script ###############################################################################
 if __name__ == '__main__':
     ret = 3
-    parser = argparse.ArgumentParser(description="""
+    parser = argparse.ArgumentParser(
+        description="""
         Nagios plugin used to return machine network interfaces stats from ElasticSearch.
         """,
         usage="""
         """,
-        epilog="version {}, copyright {}".format(__version__, __copyright__))
+        epilog="version {}, copyright {}".format(__version__, __copyright__)
+    )
     parser.add_argument('-H', '--hostname', type=str, help='hostname or IP address', required=True)
     parser.add_argument('-t', '--timeout', type=int, help='data validity timeout (in minutes)', default=4)
-    parser.add_argument('-E', '--elastichost', type=str, help='connection URL of ElasticSearch server', default="http://localhost:9200")
+    parser.add_argument(
+        '-E', '--elastichost', type=str, help='connection URL of ElasticSearch server',
+        default="http://localhost:9200"
+    )
     parser.add_argument('-v', '--verbose', help='be verbose', action='store_true')
-
     args = parser.parse_args()
-    
+
     if validate_elastichost(args.elastichost):
         lstnic = get_interfaces(args.elastichost, args.hostname, args.timeout, args.verbose)
         if isinstance(lstnic, list) and len(lstnic) > 0:
-            print("OK: {} | {}".format(
-                ", ".join([ "interface {} is up".format(i['name']) for i in lstnic ]),
-                " ".join([ "'{nic}_in_octet'={oin}c '{nic}_out_octet'={out}c".format(nic=i['name'], oin=i['in']['bytes'], out=i['out']['bytes']) for i in lstnic ])
-            ))
+            print(
+                "OK: {} | {}".format(
+                    ", ".join(["interface {} is up".format(i['name']) for i in lstnic]),
+                    " ".join(
+                        [
+                            "'{nic}_in_octet'={oin}c '{nic}_out_octet'={out}c".format(
+                                nic=i['name'],
+                                oin=i['in']['bytes'],
+                                out=i['out']['bytes']
+                            ) for i in lstnic
+                        ]
+                    )
+                )
+            )
             ret = 0
         else:
             print("UNKNOWN: no interface returned")
